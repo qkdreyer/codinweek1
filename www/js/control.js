7e0ca4317f4c4e62401b40a201f511da99f9085b
Jouby <you@example.com>
2014-03-29 10:41:57 +0100

    'controlplayer'

--- www/js/control.js
+++ www/js/control.js
@@ -13,33 +13,34 @@ var control =
 
     initMoveButton: function()
     {
+        var self = this;
         // Create action buttons
-        buttons.enableBody = false;
+        this.buttons.enableBody = false;
 
-        var upButton = buttons.create(20, game.height-40, 'star');
-        var leftButton = buttons.create(0, game.height-20, 'star');
-        var rightButton = buttons.create(40, game.height-20, 'star');
+        var upButton = this.buttons.create(20, game.height-40, 'star');
+        var leftButton = this.buttons.create(0, game.height-20, 'star');
+        var rightButton = this.buttons.create(40, game.height-20, 'star');
 
         upButton.inputEnabled = true;
         upButton.events.onInputDown.add(function(){
-            moveButton = 'up';
+            self.moveButton = 'up';
         });
         leftButton.inputEnabled = true;
         leftButton.events.onInputDown.add(function(){
-            moveButton = 'left';
+            self.moveButton = 'left';
         });
         rightButton.inputEnabled = true;
         rightButton.events.onInputDown.add(function(){
-            moveButton = 'right';
+            self.moveButton = 'right';
         });
         leftButton.events.onInputUp.add(function(){
-            moveButton = '';
+            self.moveButton = '';
         });
         rightButton.events.onInputUp.add(function(){
-            moveButton = '';
+            self.moveButton = '';
         });
         upButton.events.onInputUp.add(function(){
-            moveButton = '';
+            self.moveButton = '';
         });
 
     }
