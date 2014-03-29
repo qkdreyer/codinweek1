f3fddc6fc661b6a1d8c437ab79805b6c479f53b5
Jouby <you@example.com>
2014-03-29 04:15:09 +0100

    'a'

--- platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
+++ platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-c:\Users\Quentin\Dev\codinweek1\platforms\android\ant-build\HelloWorld-debug-unaligned.apk : \
-c:\Users\Quentin\Dev\codinweek1\platforms\android\ant-build\HelloWorld.ap_ \
-c:\Users\Quentin\Dev\codinweek1\platforms\android\ant-build\classes.dex \
-C:\Users\Quentin\Dev\codinweek1\platforms\android\CordovaLib\ant-build\classes.jar \
+C:\wamp\www\codinweek1\platforms\android\ant-build\HelloWorld-debug-unaligned.apk : \
+C:\wamp\www\codinweek1\platforms\android\ant-build\HelloWorld.ap_ \
+C:\wamp\www\codinweek1\platforms\android\ant-build\classes.dex \
+C:\wamp\www\codinweek1\platforms\android\CordovaLib\ant-build\classes.jar \
